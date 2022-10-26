e68962945c7c21036a642e66601e45d62ce0b7cc
Samuel Ralak <thesamuelralak@gmail.com>
2022-10-26 15:28:11 +0300

    make it so :rocket:

---
+++ lib/chpter/rb/version.rb
@@ -0,0 +1,5 @@
+module Chpter
+  module Rb
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/chpter/rb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Chpter::Rb do
+  it "has a version number" do
+    expect(Chpter::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
